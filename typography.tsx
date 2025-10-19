7278c5049a09d441048222d5a268b6ce6fecd15e
ALADETAN-IFE <163364168+ALADETAN-IFE@users.noreply.github.com>
2025-10-19 02:09:05 +0100

    Refactor className handling in Typography component
    Did a conditional rendering to avoid className displaying as undefined

--- typography.tsx
+++ typography.tsx
@@ -1,11 +1,11 @@
 export default function Typography({variant, className, children}: {variant: "h1" | "h2" | "h3" | "h4" | "p", className?: string, children: React.ReactNode}) {
     return (
         <>
-            {variant === "h1" && <h1 className={`text-4xl font-bold ${className}`}>{children}</h1>}
-            {variant === "h2" && <h2 className={`text-3xl font-bold ${className}`}>{children}</h2>}
-            {variant === "h3" && <h3 className={`text-2xl font-bold ${className}`}>{children}</h3>}
-            {variant === "h4" && <h4 className={`text-xl font-bold ${className}`}>{children}</h4>}
-            {variant === "p" && <p className={`text-lg ${className}`}>{children}</p>}
+            {variant === "h1" && <h1 className={`text-4xl font-bold ${className ? className : ""}`}>{children}</h1>}
+            {variant === "h2" && <h2 className={`text-3xl font-bold ${className ? className : ""}`}>{children}</h2>}
+            {variant === "h3" && <h3 className={`text-2xl font-bold ${className ? className : ""}`}>{children}</h3>}
+            {variant === "h4" && <h4 className={`text-xl font-bold ${className ? className : ""}`}>{children}</h4>}
+            {variant === "p" && <p className={`text-lg ${className ? className : ""}`}>{children}</p>}
         </>
     );
 }
